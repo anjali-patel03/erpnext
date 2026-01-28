437ae9a89fdbb9decea800b249210d7e746268a6
Anjali Patel <anjali.patel@artemhealthtech.com>
2026-01-28 11:23:35 +0530

    core: register custom ERPNext hooks

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -14,7 +14,7 @@ add_to_apps_screen = [
 	{
 		"name": app_name,
 		"logo": "/assets/erpnext/images/erpnext-logo.svg",
-		"title": app_title,
+		"title": "BMC HR",
 		"route": app_home,
 		"has_permission": "erpnext.check_app_permission",
 	}
